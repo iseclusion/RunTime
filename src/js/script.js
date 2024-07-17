bec585cdaaa7ccf642c64dad42276224dacbb842
Ilkhan <sanatullin2002@gmail.com>
2024-07-17 14:17:30 +0300

    create_slayder

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="left_arrow"></img></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../icons/right.svg" alt="right_arrow"></img></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
