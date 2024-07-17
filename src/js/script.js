50dcc25e7d30a3e5451fbc45d76405ef0eaab368
Ilkhan <sanatullin2002@gmail.com>
2024-07-17 14:31:32 +0300

    script_must_work

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     speed: 1200,
     adaptiveHeight: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="left_arrow"></img></button>',
+      '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="left_arrow"></img></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="../icons/right.svg" alt="right_arrow"></img></button>',
+      '<button type="button" class="slick-next"><img src="icons/right.svg" alt="right_arrow"></img></button>',
     responsive: [
       {
         breakpoint: 992,
